c69dd6499baa4d35f2af6b0dca1206cc18519f10
MainaJoseph <mainajm254@gmail.com>
2024-10-24 14:58:26 +0300

    created dashbord page to check for authentication

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,18 @@
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
-const DashboardPage = (props: Props) => {
+const DasboardPage = async (props: Props) => {
   //Authentication
+  const auth = await onAuthenticateUser();
+  if (auth.status === 200 || auth.status === 201)
+    return redirect(`/dashboard/${auth.user?.workspace[0].id}`);
 
-  //If account does not exist
-
-  return <div>DashboardPage</div>;
+  if (auth.status === 400 || auth.status === 500 || auth.status === 404) {
+    return redirect("/auth/sign-in");
+  }
 };
 
-export default DashboardPage;
+export default DasboardPage;
