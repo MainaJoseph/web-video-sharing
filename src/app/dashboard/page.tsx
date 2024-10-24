cd60c56d53a4cb776da02dca86e821583133746b
MainaJoseph <mainajm254@gmail.com>
2024-10-24 13:59:50 +0300

    created server actiions user-server actions and a lib prisma.ts

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+  //Authentication
+
+  //If account does not exist
+
+  return <div>DashboardPage</div>;
+};
+
+export default DashboardPage;
