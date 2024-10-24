111dd4740f7d0eb0ad225f9ff121ccfaba1bc544
MainaJoseph <mainajm254@gmail.com>
2024-10-24 22:32:32 +0300

    created workplace-placeholder

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -22,6 +22,7 @@ import Search from "../search";
 import { MENU_ITEMS } from "@/constants";
 import SidebarItem from "./sidebar-item";
 import { getNotifications } from "@/actions/user";
+import WorkspacePlaceholder from "./workspace-placeholder";
 
 type Props = {
   activeWorkspaceId: string;
@@ -145,6 +146,55 @@ const Sidebar = ({ activeWorkspaceId }: Props) => {
               }
             />
           ))}
+        </ul>
+      </nav>
+      <Separator className="w-4/5" />
+      <p className="w-full text-[#9D9D9D] font-bold mt-4 ">Workspaces</p>
+
+      {workspace.workspace.length === 1 && workspace.members.length === 0 && (
+        <div className="w-full mt-[-10px]">
+          <p className="text-[#3c3c3c] font-medium text-sm">
+            {workspace.subscription?.plan === "FREE"
+              ? "Upgrade to create workspaces"
+              : "No Workspaces"}
+          </p>
+        </div>
+      )}
+      <nav className="w-full">
+        <ul className="h-[150px] overflow-auto overflow-x-hidden fade-layer">
+          {workspace.workspace.length > 0 &&
+            workspace.workspace.map(
+              (item) =>
+                item.type !== "PERSONAL" && (
+                  <SidebarItem
+                    href={`/dashboard/${item.id}`}
+                    selected={pathName === `/dashboard/${item.id}`}
+                    title={item.name}
+                    notifications={0}
+                    key={item.name}
+                    icon={
+                      <WorkspacePlaceholder>
+                        {item.name.charAt(0)}
+                      </WorkspacePlaceholder>
+                    }
+                  />
+                )
+            )}
+          {workspace.members.length > 0 &&
+            workspace.members.map((item) => (
+              <SidebarItem
+                href={`/dashboard/${item.WorkSpace.id}`}
+                selected={pathName === `/dashboard/${item.WorkSpace.id}`}
+                title={item.WorkSpace.name}
+                notifications={0}
+                key={item.WorkSpace.name}
+                icon={
+                  <WorkspacePlaceholder>
+                    {item.WorkSpace.name.charAt(0)}
+                  </WorkspacePlaceholder>
+                }
+              />
+            ))}
         </ul>
       </nav>
     </div>
