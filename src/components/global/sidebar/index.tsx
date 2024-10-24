a0cb83317ec2fec8b35b757075e0bd3dc81725e5
MainaJoseph <mainajm254@gmail.com>
2024-10-24 17:21:57 +0300

    created custom hook useQueryData

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -1,9 +1,52 @@
+"use client";
+
+import Image from "next/image";
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
 
-type Props = {};
-
-const SideBar = (props: Props) => {
-  return <div>SideBar</div>;
+type Props = {
+  activeWorkspaceId: string;
 };
 
-export default SideBar;
+const Sidebar = ({ activeWorkspaceId }: Props) => {
+  const router = useRouter();
+
+  const onChangeActiveWorkspace = (value: string) => {
+    router.push(`/dashboard/${value}`);
+  };
+
+  return (
+    <div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
+      <div className="bg-[#111111] p-4 flex gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0 ">
+        <Image src="/nova-logo.svg" height={40} width={40} alt="logo" />
+        <p className="text-2xl">Nova</p>
+      </div>
+      <Select
+        defaultValue={activeWorkspaceId}
+        onValueChange={onChangeActiveWorkspace}
+      >
+        <SelectTrigger className="mt-16 text-neutral-400 bg-transparent">
+          <SelectValue placeholder="Select a workspace">
+            Select a workspace
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent className="bg-[#111111] backdrop-blur-xl">
+          <SelectGroup>
+            <SelectLabel>Workspaces</SelectLabel>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default Sidebar;
