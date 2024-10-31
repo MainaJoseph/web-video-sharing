15ecd6b3b13a68cdd5839ab1145a04a087efc15e
MainaJoseph <mainajm254@gmail.com>
2024-10-31 03:17:21 +0300

    Fixed light mode and darkmode CommentForm

--- src/components/forms/comment-form/index.tsx
+++ src/components/forms/comment-form/index.tsx
@@ -1,29 +1,26 @@
-'use client'
-import FormGenerator from '@/components/global/form-generator'
-import Loader from '@/components/global/loader'
-import { Button } from '@/components/ui/button'
-import { useVideoComment } from '@/hooks/useVideo'
-import { Send, X } from 'lucide-react'
-import React from 'react'
+"use client";
+import FormGenerator from "@/components/global/form-generator";
+import Loader from "@/components/global/loader";
+import { Button } from "@/components/ui/button";
+import { useVideoComment } from "@/hooks/useVideo";
+import { Send, X } from "lucide-react";
+import React from "react";
 
 type Props = {
-  videoId: string
-  commentId?: string
-  author: string
-  close?: () => void
-}
+  videoId: string;
+  commentId?: string;
+  author: string;
+  close?: () => void;
+};
 
 const CommentForm = ({ author, videoId, close, commentId }: Props) => {
   const { errors, isPending, onFormSubmit, register } = useVideoComment(
     videoId,
     commentId
-  )
+  );
 
   return (
-    <form
-      className="relative w-full"
-      onSubmit={onFormSubmit}
-    >
+    <form className="relative w-full" onSubmit={onFormSubmit}>
       <FormGenerator
         register={register}
         errors={errors}
@@ -34,18 +31,21 @@ const CommentForm = ({ author, videoId, close, commentId }: Props) => {
         type="text"
       />
       <Button
-        className="p-0 bg-transparent absolute top-[1px] right-3 hover:bg-transparent "
+        className="p-0 bg-transparent absolute top-[1px] right-3 
+          hover:bg-transparent"
         type="submit"
       >
         <Loader state={isPending}>
           <Send
-            className="text-white/50 cursor-pointer hover:text-white/80"
+            className="text-zinc-400 dark:text-zinc-500 
+              hover:text-zinc-600 dark:hover:text-zinc-300
+              transition-colors"
             size={18}
           />
         </Loader>
       </Button>
     </form>
-  )
-}
+  );
+};
 
-export default CommentForm
+export default CommentForm;
