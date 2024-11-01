89f9860792ca4a04cff5c1f4552f0f4cb794d3e0
MainaJoseph <mainajm254@gmail.com>
2024-11-01 15:33:12 +0300

    Wired up got first video view

--- src/actions/workspace.ts
+++ src/actions/workspace.ts
@@ -2,7 +2,7 @@
 
 import { client } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
-// import { sendEmail } from "./user";
+import { sendEmail } from "./user";
 import { createClient, OAuthStrategy } from "@wix/sdk";
 import { items } from "@wix/data";
 import axios from "axios";
@@ -560,69 +560,69 @@ export const getPreviewVideo = async (videoId: string) => {
  * - {status: 200, data: video, author: boolean} if successful
  * - {status: 400} if query fails
  */
-// export const sendEmailForFirstView = async (videoId: string) => {
-//   try {
-//     const user = await currentUser();
-//     if (!user) return { status: 404 };
-//     const firstViewSettings = await client.user.findUnique({
-//       where: { clerkid: user.id },
-//       select: {
-//         firstView: true,
-//       },
-//     });
-//     if (!firstViewSettings?.firstView) return;
+export const sendEmailForFirstView = async (videoId: string) => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+    const firstViewSettings = await client.user.findUnique({
+      where: { clerkid: user.id },
+      select: {
+        firstView: true,
+      },
+    });
+    if (!firstViewSettings?.firstView) return;
 
-//     const video = await client.video.findUnique({
-//       where: {
-//         id: videoId,
-//       },
-//       select: {
-//         title: true,
-//         views: true,
-//         User: {
-//           select: {
-//             email: true,
-//           },
-//         },
-//       },
-//     });
-//     if (video && video.views === 0) {
-//       await client.video.update({
-//         where: {
-//           id: videoId,
-//         },
-//         data: {
-//           views: video.views + 1,
-//         },
-//       });
+    const video = await client.video.findUnique({
+      where: {
+        id: videoId,
+      },
+      select: {
+        title: true,
+        views: true,
+        User: {
+          select: {
+            email: true,
+          },
+        },
+      },
+    });
+    if (video && video.views === 0) {
+      await client.video.update({
+        where: {
+          id: videoId,
+        },
+        data: {
+          views: video.views + 1,
+        },
+      });
 
-//       const { transporter, mailOptions } = await sendEmail(
-//         video.User?.email!,
-//         "You got a viewer",
-//         `Your video ${video.title} just got its first viewer`
-//       );
+      const { transporter, mailOptions } = await sendEmail(
+        video.User?.email!,
+        "You got a viewer",
+        `Your video ${video.title} just got its first viewer`
+      );
 
-//       transporter.sendMail(mailOptions, async (error, info) => {
-//         if (error) {
-//           console.log(error.message);
-//         } else {
-//           const notification = await client.user.update({
-//             where: { clerkid: user.id },
-//             data: {
-//               notification: {
-//                 create: {
-//                   content: mailOptions.text,
-//                 },
-//               },
-//             },
-//           });
-//           if (notification) {
-//             return { status: 200 };
-//           }
-//         }
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+      transporter.sendMail(mailOptions, async (error, info) => {
+        if (error) {
+          console.log(error.message);
+        } else {
+          const notification = await client.user.update({
+            where: { clerkid: user.id },
+            data: {
+              notification: {
+                create: {
+                  content: mailOptions.text,
+                },
+              },
+            },
+          });
+          if (notification) {
+            return { status: 200 };
+          }
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
